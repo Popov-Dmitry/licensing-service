b9a8fd13ebe7fdcb47aa832bd817347d00df2741
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-06-16 02:09:49 +0700

    Added NotifyRepository and saving in db by scheduling

--- notification-service/src/main/java/com/github/popovdmitry/notificationservice/service/NotificationService.java
+++ notification-service/src/main/java/com/github/popovdmitry/notificationservice/service/NotificationService.java
@@ -3,7 +3,10 @@ package com.github.popovdmitry.notificationservice.service;
 import com.github.popovdmitry.notificationservice.dto.LicenseInfoDTO;
 import com.github.popovdmitry.notificationservice.dto.UserInfoDTO;
 import com.github.popovdmitry.notificationservice.model.Notification;
+import com.github.popovdmitry.notificationservice.repository.NotifyRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -11,9 +14,11 @@ import java.util.Map;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class NotificationService {
 
-    private Map<Long, Notification> notificationMap = new HashMap<>();
+    private final NotifyRepository notifyRepository;
+    private final Map<Long, Notification> notificationMap = new HashMap<>();
 
     public Long saveLicenseInfo(LicenseInfoDTO licenseInfoDTO) {
         Notification notification = new Notification();
@@ -28,6 +33,12 @@ public class NotificationService {
         notification.setUserName(userInfoDTO.getUserName());
         notification.setEmail(userInfoDTO.getEmail());
         notificationMap.put(Long.parseLong(id), notification);
+    }
+
+    @Scheduled(cron = "0 0 4 * * ?")
+    private void saveNotifications() {
+        notifyRepository.deleteAll();
+        notificationMap.forEach((k, v) -> notifyRepository.save(v));
     }
 
 }
