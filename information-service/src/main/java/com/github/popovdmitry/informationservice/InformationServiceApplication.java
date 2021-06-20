e4760a5b2b1e5013935af632c02404a70e6ad908
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-06-20 18:17:38 +0700

    Init information service

---
+++ information-service/src/main/java/com/github/popovdmitry/informationservice/InformationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.informationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationServiceApplication.class, args);
+    }
+
+}
---
+++ information-service/src/test/java/com/github/popovdmitry/informationservice/InformationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.popovdmitry.informationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
